82d5d91846940b5c0bb035210cb66993fc4944a3
matteozani <mattezani2003@gmail.com>
2021-03-04 15:10:31 +0100

    Revert "Added a mistake on purpose!"
    This reverts commit 7eabaabe99dad0070527deb4b43f34ccb8b0d7ae.

--- letsgo.cpp
+++ letsgo.cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-cout<< "Git. lets go!"<<endl;
-cout<< "Added line 1" <<endl;
-cout<< "Added another mistake" <<endl;
-return 0;
-}
+ ujgsaiuydc vwet csdajkwegiqeguinclivudh9gdiqgd
