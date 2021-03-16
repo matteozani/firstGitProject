59909c3c5347026e1a2a8d22a5fbd0be05a48233
matteozani <mattezani2003@gmail.com>
2021-03-16 19:56:01 +0100

    Added comment from rep2-mach2

--- letsgo.cpp
+++ letsgo.cpp
@@ -1,1 +1,12 @@
- ujgsaiuydc vwet csdajkwegiqeguinclivudh9gdiqgd
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout<<"Git, lets go " <<endl;
+cout<<"Added line 1"<<endl;
+cout<<2working on a new feature 1..."<<endl;
+return 0;
+}
+/* Added comment from repository 2 (machine 2) */
